73788bd2685163c13ac32e27da15b3e92c477d09
uzquiano <mgu1@cornell.edu>
2014-09-29 16:58:28 -0400

    update to index

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ var fs = require("fs");
 
 // default settings so that we connect to Cloud CMS demo sandbox (by default)
 Gitana.DEFAULT_CONFIG = {
-	"clientKey": "676e3450-6131-46c2-99cc-496aa2ad80fa",
-	"clientSecret": "5fGkvesH/tWEMX6SpevL54rY6iJK5ADzLH963sif2ljrWvFOhV2zXv6rSpLF2uMWlJ9SG0uEO9uQO4JZac0i7DZquA/5W8ixJwhj76g0Ksk=",
-	"baseURL": "https://api.cloudcms.com",
-	"username": "demo",
-	"password": "demo"
+	"baseURL": "https://api.cloudcms.com"
 };
 
 // tell Gitana driver to load settings from an optional "gitana.json" file
