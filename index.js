a68a7b88265f169fefa664500d90c9c1e5ecfa0f
uzquiano <mgu1@cornell.edu>
2015-05-12 13:19:05 -0400

    update to latest release

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var Gitana = require("./lib/gitana");
 var fs = require("fs");
+var path = require("path");
 
 // default settings so that we connect to Cloud CMS demo sandbox (by default)
 Gitana.DEFAULT_CONFIG = {
 	"baseURL": "https://api.cloudcms.com"
 };
 
-var defaultConfig = null;
-if (fs.existsSync(__dirname + "/../../gitana.json")) {
-	defaultConfig = JSON.parse(fs.readFileSync(__dirname + "/../../gitana.json"));
-}
-
 // tell Gitana driver to load settings from an optional "gitana.json" file
 Gitana.loadDefaultConfig = function() {
-    return defaultConfig;
+
+	var configFilePath = path.resolve(path.join(".", "gitana.json"));
+
+	var defaultConfig = null;
+	if (fs.existsSync(configFilePath)) {
+		defaultConfig = JSON.parse(fs.readFileSync(configFilePath));
+	}
+
+	console.log("DEFAULT CONFIG: " + JSON.stringify(defaultConfig));
+		
+	return defaultConfig;
 };
 
 module.exports = Gitana;
